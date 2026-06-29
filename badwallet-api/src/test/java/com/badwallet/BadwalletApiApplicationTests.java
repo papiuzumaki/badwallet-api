abdfa8724366e1c527f1b08ea27745d959e0f556
Pape Oumar Ndiaye <ndiayepapeoumar85@gmail.com>
2026-06-29 10:03:00 +0000

    feat: add badwallet-api project files and UI test interface

---
+++ badwallet-api/src/test/java/com/badwallet/BadwalletApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.badwallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BadwalletApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
